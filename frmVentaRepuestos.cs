e6a789f8e967ff1b1c74168b2352a842ae91aff0
simonabaldovino <baldovinosimona020@gmail.com>
2025-09-24 12:45:59 -0300

    Agregar archivos de proyecto.

---
+++ frmVentaRepuestos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryBaldovinoSP3ExR
+{
+    public partial class frmVentaRepuestos : Form
+    {
+        public frmVentaRepuestos()
+        {
+            InitializeComponent();
+        }
+    }
+}
